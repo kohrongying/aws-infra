d986420d24e478988df102d56b071f4280cf9a67
rongying <kohrongying@gmail.com>
2022-05-18 18:12:31 +0800

    Add project and new table.

--- modules/basic/dynamo-db/main.tf
+++ modules/basic/dynamo-db/main.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "this" {
   name           = var.name
   billing_mode   = var.billing_mode
-  read_capacity  = 20
-  write_capacity = 20
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
   hash_key       = var.hash_key
   range_key      = var.range_key
 
--- modules/basic/dynamo-db/variables.tf
+++ modules/basic/dynamo-db/variables.tf
@@ -6,6 +6,10 @@ variable "name" {}
 
 variable "billing_mode" {}
 
+variable "read_capacity" {}
+
+variable "write_capacity" {}
+
 variable "hash_key" {}
 
 variable "range_key" {}
